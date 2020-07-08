54c5cef6995f67ae4ecbd5ee3b7894d8e1211e63
Michael <micha3lheitmann@gmail.com>
2020-07-08 15:45:11 -0500

    save

---
+++ climate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[54]:
+
+
+import matplotlib as mpl
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"]=20,10
+
+
+# In[55]:
+
+
+data = np.genfromtxt('Matplotlib-Numpy-Climate-Data\Climate_Data.csv', delimiter=',', dtype=None, skip_header=5, names=('date', 'value', 'anomaly'))
+
+
+# In[56]:
+
+
+plt.title('Global Land and Ocean Temperature Anomalies, June')
+plt.xlabel('year')
+plt.ylabel('degrees F +/- from average')
+plt.bar(data['date'], data['value'], color='blue')
+plt.show()
+
+
+# In[50]:
+
+
+# change the cell width
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:100% !important; }</style>"))
+
